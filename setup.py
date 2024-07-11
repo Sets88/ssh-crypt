76b6eb37716e398faa7e7b9dd51d0ab09420bc33
Maxim Nikitenko <iam@sets88.com>
2024-07-11 09:43:06 +0300

    do not crush in cases when ssh-agent is not available, or sign not confirmed in fifo mode

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_requirements():
 
 setup(
     name='ssh-crypt',
-    version='1.1.10',
+    version='1.1.11',
     license='BSD',
     author='Maxim Nikitenko',
     author_email='iam@sets88.com',
